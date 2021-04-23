518f14ff929d253d93124030a1cdda6c406f96d1
Rayhane <bousninarayhanesimplonenda@gmail.com>
2021-04-23 15:27:58 +0100

    navbar and first section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import Navigation from './components/navbar';
+import CardOne from './components/card1';
+import {Row, Col, Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Navigation/>
+      <Row className="d-flex justify-content-center"><img src="images/sygnet.png"></img></Row>
+      <Row className="d-flex justify-content-center pt-5">        
+        <div className="title h1">
+          Making the healthcare experience more human
+        </div>
+      </Row>
+      <Container>
+        <Row className="my-5">
+            <Col md={6}><p>We want patients to find the perfect doctor and book an appointment in the most easy way. The patient journey should be enjoyable, and that's why we are always next to them: to help them find the best possible care. Anytime, anywhere.</p></Col>
+            <Col md={6}><p>We also help doctors to better manage their practice and build their online reputation. With our integrated end-to-end solution, doctors are able not only to improve their online presence, but also to devote their time to what really matters: their patients.</p></Col>
+        </Row>
+      </Container>
+
+      <Container>     
+        <Row className="">
+          <Col><CardOne/></Col>
+          <Col><CardOne/></Col>
+          <Col><CardOne/></Col>
+        </Row>
+      </Container>
     </div>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import {Navbar, Nav} from 'react-bootstrap';
+import {navbar} from '../css/navbar.css';
+function Navigation() {
+    return (
+        <div>
+            <Navbar expand="lg" className="navigation pb-5">
+                <Navbar.Brand href="#home"><img class="logo-docplanner" src="images/logo-default-group-en.svg" alt="Docplanner Group"></img></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link className="link" href="#home">About us</Nav.Link>
+                        <Nav.Link className="link" href="#career">Career</Nav.Link>     
+                        <Nav.Link className="link" href="#departments">Departments</Nav.Link>             
+                    </Nav>                
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navigation;
